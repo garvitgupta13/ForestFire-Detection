1726e2dd5d179027b25e566180f3e6de324a7c23
Garvit Gupta <garvitgupta2001@gmail.com>
2022-07-02 22:21:47 +0530

    First commit

---
+++ extract-frames.py
@@ -0,0 +1,76 @@
+from datetime import timedelta
+import cv2
+import numpy as np
+import os
+
+SAVING_FRAMES_PER_SECOND = 10
+
+
+def format_timedelta(td):
+    """Utility function to format timedelta objects in a cool way (e.g 00:00:20.05) 
+    omitting microseconds and retaining milliseconds"""
+    result = str(td)
+    try:
+        result, ms = result.split(".")
+    except ValueError:
+        return result + ".00".replace(":", "-")
+    ms = int(ms)
+    ms = round(ms / 1e4)
+    return f"{result}.{ms:02}".replace(":", "-")
+
+
+def get_saving_frames_durations(cap, saving_fps):
+    """A function that returns the list of durations where to save the frames"""
+    s = []
+    clip_duration = cap.get(cv2.CAP_PROP_FRAME_COUNT) / \
+        cap.get(cv2.CAP_PROP_FPS)
+    for i in np.arange(0, clip_duration, 1 / saving_fps):
+        s.append(i)
+    return s
+
+
+def main(video_file):
+    filename, _ = os.path.splitext(video_file)
+    filename += "-frames"
+
+    if not os.path.isdir(filename):
+        os.mkdir(filename)
+
+    cap = cv2.VideoCapture(video_file)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    saving_frames_per_second = min(fps, SAVING_FRAMES_PER_SECOND)
+    saving_frames_durations = get_saving_frames_durations(
+        cap, saving_frames_per_second)
+
+    count = 0
+    while True:
+        is_read, frame = cap.read()
+
+        if not is_read:
+            break
+
+        frame_duration = count / fps
+
+        try:
+            closest_duration = saving_frames_durations[0]
+        except IndexError:
+            break
+
+        if frame_duration >= closest_duration:
+            frame_duration_formatted = format_timedelta(
+                timedelta(seconds=frame_duration))
+
+            cv2.imwrite(os.path.join(
+                filename, f"{filename[:2]}-{frame_duration_formatted}.jpg"), frame)
+            try:
+                saving_frames_durations.pop(0)
+            except IndexError:
+                pass
+        count += 1
+
+
+if __name__ == "__main__":
+    import sys
+    video_file = sys.argv[1]
+    main(video_file)
